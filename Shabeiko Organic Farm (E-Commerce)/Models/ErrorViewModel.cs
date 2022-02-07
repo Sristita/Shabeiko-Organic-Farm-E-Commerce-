851da26640a9e329e056d29a64326359e48ef2df
DESKTOP-IBGVRJ1\Unknown <kunwarsristita321@gmail.com>
2022-02-07 21:44:13 +0545

    Add project files.

---
+++ Shabeiko Organic Farm (E-Commerce)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shabeiko_Organic_Farm__E_Commerce_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
